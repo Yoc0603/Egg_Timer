860f21608c672a99fea893e967a62a0a88246423
Yavuz Orhan Candemir <yavuzorhancandemir@gmail.com>
2025-05-31 13:24:56 +0200

    first push

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,15 +1,40 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progresBar: UIProgressView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimes = ["Yumusak": 3, "Orta": 6, "Sert": 9]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
 
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        progresBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progresBar.progress = Float(secondsPassed) / Float(totalTime)
+
+        } else{
+            timer.invalidate()
+            titleLabel.text = "BITTI!"
+        }
+    }
 
 }
